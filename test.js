4b5bbb513109b9f1a6934143aa8ea6fd75737a81
Dhanuka_Rathnayaka <143099797+DhanukaRathnayaka@users.noreply.github.com>
2024-06-15 23:13:06 +0530

    Update test.js

--- test.js
+++ test.js
@@ -11,7 +11,7 @@ document.getElementById("js-page-loading_start").addEventListener("click", () =>
         const increment = target / 2000; // Adjust this value to change the increment speed
         if (count < target) {
           counter.innerText = `${Math.ceil(count + increment)}`;
-          setTimeout(updateCounter, 50); // Adjust this value to change the delay
+          setTimeout(updateCounter, 20); // Adjust this value to change the delay
         } else {
           counter.innerText = `${target}+`;
         }
